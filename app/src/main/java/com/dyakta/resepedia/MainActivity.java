faa74d9dc7ab5cadcd439e22a4bb88a0b14df187
Ridloyanuar2 <Ridloyanuar@dyakta.com>
2020-02-10 23:30:28 -0800

    first commit

---
+++ app/src/main/java/com/dyakta/resepedia/MainActivity.java
@@ -0,0 +1,27 @@
+package com.dyakta.resepedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
